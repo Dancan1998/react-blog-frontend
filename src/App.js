3238b7c5ebb2a4b5c360e4f51c13c27cd2006fc7
Dancan Chibole <dancankingstar@gmail.com>
2022-04-01 13:31:35 +0300

    Register page

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import Home from "./pages/home/home";
 import Single from "./pages/single/Single";
 import Profile from "./pages/profile/Profile";
 import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
 
 function App() {
   return (
     <>
       <TopBar />
-      <Login />
+      <Register />
     </>
   );
 }
