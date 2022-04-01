a201668e67dff29709f1cea4a67a7b7581be25dc
Dancan Chibole <dancankingstar@gmail.com>
2022-04-01 15:36:44 +0300

    testing

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div class="hero-image">
       <div class="hero-text">
-        <h3 className="headerTitles">React & DJango</h3>
+        <h3 className="headerTitles">React Powered By DJango</h3>
         {/* <h3>And I'm a Photographer</h3>
         <button>Read More</button> */}
       </div>
